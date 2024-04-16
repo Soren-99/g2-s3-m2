8d2c873cd0df0ac8e7c42f030df64b711a128341
Persiano99 <sorenrahimi99@gmail.com>
2024-04-16 16:18:40 +0200

    Initial commit

---
+++ src/main/java/sorenrahimi/g2s3m2/G2S3M2Application.java
@@ -0,0 +1,13 @@
+package sorenrahimi.g2s3m2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2S3M2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G2S3M2Application.class, args);
+	}
+
+}
